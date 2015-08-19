625319310015632132461ce3b27965480255c00e
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-19 09:45:14 -0600

    adding new game variables

--- js/utility.js
+++ js/utility.js
@@ -3,6 +3,13 @@ var Game = function(title, genre) {
   this.genre = genre;
 }
 
+Game.prototype.render = function() {
+  var gameItem = $('<div>')
+  libraryItems.append('<li>' + this.games[i].title + '</li>');
+  libraryItems.append('<li>' + this.games[i].genre + '</li>');
+  libraryItems.append('<p></p>');
+}
+
 var GameLibrary = function(title) {
   this.title = title;
   this.games = [];
@@ -14,6 +21,8 @@ GameLibrary.prototype.render = function() {
   for (var i = 0; i < this.games.length; i++) {
     libraryItems.append('<li>' + this.games[i].title + '</li>');
     libraryItems.append('<li>' + this.games[i].genre + '</li>');
+    libraryItems.append('<p></p>');
+
   };
   libraryItems.append('</div>');
 
@@ -23,5 +32,13 @@ GameLibrary.prototype.render = function() {
 GameLibrary.prototype.addGame = function(newGame) {
   this.games.push(newGame);
 }
+
+
+var bingo = new Game('Title: bingo', 'Genre: grandma');
+var cribbage = new Game ('Title: cribbage', 'Genre: grandpa');
+var OldPeople = new GameLibrary('Old People Games');
+OldPeople.addGame(bingo);
+OldPeople.addGame(cribbage);
+OldPeople.render();
 
 
