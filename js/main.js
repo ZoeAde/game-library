549da8f308717848f5e0047e6c90587cf59af804
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-25 11:54:34 -0600

    Adding table to new library data

--- js/main.js
+++ js/main.js
@@ -4,45 +4,53 @@ $(document).on('ready', function() {
 var titleInput = $('.titleInput');
 var genreInput = $('.genreInput');
 var libraryInput = $('.libraryName');
+
 //only show library options to start
 $('.gameForm').hide();
 
 //When you add a library, show the game form and create the library title
 $('.libraryBtn').on("click", function(event) {
   event.preventDefault();
-  //hide library
+
   $('.libraryForm').hide();
   $('.gameForm').show();
 
   //add title
   var titlePlacement = $('.libraryTitle');
+  var libraryList = $('.librarySelection');
+  var librarySelection = libraryList.val();
   var libraryInput = $('.libraryInput').val();
-  var librarySelection = $('.librarySelection').val();
 
   //if input box isnt empty, use that value and append to screen, append to option list
   if (libraryInput !== "") {
-    titlePlacement.append("<h2 class='libTitle'>" + libraryInput + "</h2>");
-    console.log('not empty input');
+    titlePlacement.append("<h2 class='libTitle newItem'>" + libraryInput + "</h2>");
+    libraryList.append("<option>" + libraryInput + "</option>");
   }
   else if (librarySelection !== "Select Library") {
-    console.log('selected from list');
-    titlePlacement.append("<h2 class='libTitle'>" + librarySelection + "</h2>");
+    titlePlacement.append("<h2 class='libTitle newItem'>" + librarySelection + "</h2>");
   }
   else {
     console.log("Please select a Library");
   }
 });
 
+//when adding a new game, append it beneath the library Title
 $('.gameBtn').on("click", function(event) {
   event.preventDefault();
-    var title = $('.titleInput').val();
-    var genre = $('.genreInput').val();
-    var game = new Game(title, genre);
-    console.log(game);
-    var addGame = game.render();
-    $('.libraryGames').append(addGame);
+  var title = $('.titleInput').val();
+  var genre = $('.genreInput').val();
+  var game = new Game(title, genre);
+  console.log(game);
+  var addGame = game.render();
+  $('.libraryGames').append(addGame);
 });
 
+$('.returnBtn').on("click", function(event) {
+  event.preventDefault();
+  $('.libraryForm').show();
+  $('.gameForm').hide();
+
+});
 
 });
 
--- js/utility.js
+++ js/utility.js
@@ -1,35 +1,17 @@
+var libraryGames = $('.libraryGames');
+
 var Game = function(title, genre) {
   this.title = title;
   this.genre = genre;
-}
+};
 
-var libraryGames = $('.libraryGames');
 Game.prototype.render = function() {
-  libraryGames.append('<li>Title: ' + this.title + '</li>');
-  libraryGames.append('<li>Genre: ' + this.genre + '</li>');
-  libraryGames.append('<br>');
+  libraryGames.append('<tr><td>' + this.title + '</td><td>' + this.genre + '</td><tr>');
 };
 
 var GameLibrary = function(title) {
   this.title = title;
   this.games = [];
-};
-
-GameLibrary.prototype.nameLibrary = function(title) {
-
-};
-
-GameLibrary.prototype.render = function() {
-  var libraryItems = $('.libraryTitle');
-  libraryItems.append('<h3>' + this.title + '</h3>');
-  for (var i = 0; i < this.games.length; i++) {
-    libraryItems.append('<li>' + this.games[i].title + '</li>');
-    libraryItems.append('<li>' + this.games[i].genre + '</li>');
-    libraryItems.append('<p></p>');
-};
-
-  // $('body').append(libraryItems);
-
 };
 
 GameLibrary.prototype.addGame = function(newGame) {
